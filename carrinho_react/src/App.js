4c9f068c4c3aa985e599596a9d1aceba90cb012d
MailsonFelipe <mfsm@icomp.ufam.edu.br>
2021-05-17 04:49:40 +0000

    nada mudou

--- carrinho_react/src/App.js
+++ carrinho_react/src/App.js
@@ -1,11 +1,31 @@
-import PaginaPrincipal from './pages/index'
+import PaginaPrincipal from './pages/index';
 
 function App() {
-  return (
+
+    const products = require('/workspace/react-challenge/carrinho_react/src/rest-api/products.json') 
+    var obj = JSON.parse(JSON.stringify(products));
+
+    var arr = products.map(function(obj) {
+    return Object.keys(obj).map(function(key) {
+        return obj[key];
+    });
+});
+console.log(arr);
+    
+  
+  return(
     <div>
-        <PaginaPrincipal/>
-    </div>
-  );
+    <PaginaPrincipal/>
+    {products.map((product) => (
+        <div>
+                <img src={product.picture} alt={product.picture}/>
+                <div>{product.title}</div>
+                <div>R$ {product.price}</div>
+            </div>
+    ))}
+
+  </div>
+  ) 
 }
 
 export default App;
--- carrinho_react/src/pages/index.js
+++ carrinho_react/src/pages/index.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { Component} from 'react';
 import './styles.css';
 
-function PaginaPrincipal(){
-    
-    return(
-        <div className="header">
-            <img className="logo" src="https://www.itriad.org.br/images/base/logo-1.png" alt="logo"></img>
-            <div className="titulo">Lojas itriad</div>
-            <button className="carrinhobtn">Carrinho</button>
-        </div>
-    )
-}
 
+class PaginaPrincipal extends Component{
+
+    viewProducts(){
+        
+        
+        alert("oi")
+        
+    }
+
+    
+render(){
+
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+            <div className="container">
+                <img className="logo" src="https://www.itriad.org.br/images/base/logo-5.png" alt="logo"></img>
+                <p className="title">Produtos</p>
+                <button className="btn" onClick={this.viewProducts}>Carrinho</button>
+            </div>
+            <div onLoad={this.viewProducts}></div>
+        </nav>
+    
+    )
+    }
+}
 export default PaginaPrincipal;
