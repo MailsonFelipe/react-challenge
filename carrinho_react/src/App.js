dac311203393c73c059149954c98fefb0f2311f2
MailsonFelipe <mfsm@icomp.ufam.edu.br>
2021-05-18 22:35:15 +0000

    ajustando imagens quebradas

--- carrinho_react/src/App.js
+++ carrinho_react/src/App.js
@@ -1,18 +1,11 @@
 import PaginaPrincipal from './pages/index';
 
 function App() {
-
-    const products = require('/workspace/react-challenge/carrinho_react/src/rest-api/products.json') 
-    var obj = JSON.parse(JSON.stringify(products));
-
-console.log(products.products);
-    
   
   return(
     <div>
-    <PaginaPrincipal/>
-
-  </div>
+        <PaginaPrincipal/>
+    </div>
   ) 
 }
 
--- carrinho_react/src/pages/index.js
+++ carrinho_react/src/pages/index.js
@@ -1,4 +1,3 @@
-import React, { useState} from 'react';
 import './styles.css';
 
 
@@ -7,27 +6,30 @@ function PaginaPrincipal(){
 var products = require('/workspace/react-challenge/carrinho_react/src/rest-api/products.json') 
 products = products.products;
 
-console.log(products)
-
     return(
         <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
-            <div className="container">
-                <img className="logo" src="https://www.itriad.org.br/images/base/logo-5.png" alt="logo"></img>
-                <p className="title">Produtos</p>
-                <button className="btn">Carrinho</button>
-            </div>
-        </nav>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+                <div className="container">
+                    <img className="logo" src="https://www.itriad.org.br/images/base/logo-5.png" alt="logo"></img>
+                    <p className="title">Produtos</p>
+                    <button className="btn">Carrinho</button>
+                </div>
+            </nav>
 
-        {products.map((product) => (
-        <div>
-            <img src={product.picture}/>
-            <h3>{product.title}</h3>
-            <h3>{product.price}</h3>
-        </div>
-        ))}
-        </div>
+            {products.map((product)  => (
+            <div>
+                <img
+                    src={product.picture}
+                    onError={(e) => {
+                        e.target.src = 'https://images-na.ssl-images-amazon.com/images/I/91MteSqsrJL.jpg'
+                    }}
+                    />
+                <h3>{product.title}</h3>
+                <h3>R$ {product.price}</h3>
+            </div>
+            ))}
+     
+       </div>
     )
-        
 }
 export default PaginaPrincipal;
